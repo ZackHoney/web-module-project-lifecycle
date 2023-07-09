fff48ba8e5764d0bc29de90508e1dc38039dce41
Zachary Honey <zhoney466@gmail.com>
2023-07-08 23:17:01 -0600

    finished project

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import Todo from './Todo'
 
 export default class TodoList extends React.Component {
   render() {
-    return ( 
-   
-    <div id='todos'>
-      <h2>Todos:</h2>
-      {
-        this.props.todos.reduce((acc, td) => {
-          if (this.props.showCompleted || !td.completed) return acc.concat(
-            <div onClick={this.props.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' 🗸' : ''}</div>
-          )
-          return acc
-        }, [])
-      }
+    return (
+
+      <div id='todos'>
+        <h2>Todos:</h2>
+        {
+          this.props.todos.reduce((acc, td) => {
+            if (this.props.showCompleted || !td.completed) return acc.concat(
+              <Todo 
+              key={td.id}
+              toggleCompleted={this.props.toggleCompleted}
+              todo={td}
+              
+              />
+            )
+            return acc
+          }, [])
+        }
       </div>
     )
   }
