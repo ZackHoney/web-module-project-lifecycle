913b5cb20d7fcb7a330150e526e2ac892bdfa84c
Zachary Honey <zhoney466@gmail.com>
2023-07-08 22:16:36 -0600

    Created Toggle Method

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -41,6 +41,17 @@ export default class App extends React.Component {
     .catch(this.setAxiosResponseError)
   }
 
+  toggleCompleted = id => () => {
+    axios.patch(`${URL}/${id}`)
+    .then(res => {
+      this.setState({...this.state, todos: this.state.todos.map(td => {
+        if(td.id !== id) return td
+        return res.data.data
+      })})
+    })
+    .catch(this.setAxiosResponseError)
+  }
+
   componentDidMount() {
     this.fetchAllTodos()
   }
@@ -53,7 +64,7 @@ export default class App extends React.Component {
           <h2>Todos:</h2>
           {
             this.state.todos.map(td => {
-              return <div key={td.id}>{td.name}</div>
+              return <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' 🗸' : ''}</div>
             })
           }
         </div>
